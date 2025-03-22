5c3b16d6eef5ac5c1727408fe784607a4ac673c6
Pankiv <o.pankiv@tenantcloud.com>
2025-03-22 14:24:57 +0200

    Revert "initial commit"
    This reverts commit 86b6783b236b35d1706a8cac2a019a5e8607d245.

--- src/main.server.ts
+++
@@ -1,9 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-
-import { config } from './app/app.config.server';
-
-import { AppComponent } from './app/app.component';
-
-const bootstrap = () => bootstrapApplication(AppComponent, config);
-
-export default bootstrap;
