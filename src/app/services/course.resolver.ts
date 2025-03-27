3c41e095ab40843e3680b19d85fc4c5ebe1670c7
Pankiv <o.pankiv@tenantcloud.com>
2025-03-27 11:45:36 +0200

    added transfer state

--- src/app/services/course.resolver.ts
+++ src/app/services/course.resolver.ts
@@ -1,5 +1,6 @@
-import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
+import { Inject, Injectable, makeStateKey, PLATFORM_ID, TransferState } from '@angular/core';
+import { isPlatformServer } from '@angular/common';
+import { ActivatedRouteSnapshot, Resolve } from '@angular/router';
 import { Observable, of } from 'rxjs';
 import { first, tap } from 'rxjs/operators';
 
@@ -9,11 +10,27 @@ import { Course } from '../model/course';
 
 @Injectable()
 export class CourseResolver implements Resolve<Course> {
-	constructor(private coursesService: CoursesService) {}
+	constructor(
+		@Inject(PLATFORM_ID) private platformId,
+		private coursesService: CoursesService,
+		private transferState: TransferState
+	) {}
 
-	resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Course> {
+	public resolve(route: ActivatedRouteSnapshot): Observable<Course> {
 		const courseId = route.params['id'];
+		const COURSE_KEY = makeStateKey<Course>(`courseKey-${courseId}`);
 
-		return this.coursesService.findCourseById(courseId).pipe(first());
+		if (this.transferState.hasKey(COURSE_KEY)) {
+			const course = this.transferState.get(COURSE_KEY, null);
+
+			this.transferState.remove(COURSE_KEY);
+
+			return of(course);
+		} else {
+			return this.coursesService.findCourseById(courseId).pipe(
+				first(),
+				tap(course => isPlatformServer(this.platformId) && this.transferState.set(COURSE_KEY, course))
+			);
+		}
 	}
 }
